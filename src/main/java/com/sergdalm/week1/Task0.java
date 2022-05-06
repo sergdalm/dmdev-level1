12722feeb62ef64f515ab4bdd8ee4c862abbecbf
Alex <30310397+sergdalm@users.noreply.github.com>
2022-05-06 16:37:56 +0300

    Initial commit

---
+++ src/main/java/com/sergdalm/week1/Task0.java
@@ -0,0 +1,98 @@
+package com.sergdalm.week1;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Имеется дата в формате "День.Месяц.Год", вводимая пользователем с консоли.
+ * Вывести дату следующего дня в формате "День.Месяц.Год".
+ */
+public class Task0 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input date in format DD.MM.YYYY and press 'Enter':");
+        String input;
+        do {
+            input = scanner.nextLine();
+        } while (!dateVerification(input));
+
+        System.out.println("Next day: " + getNextDay(input));
+    }
+
+
+    public static String getNextDay(String date) {
+        String[] dateAsStringArray = date.split("\\.");
+        int day = Integer.parseInt(dateAsStringArray[0]);
+        int month = Integer.parseInt(dateAsStringArray[1]);
+        int year = Integer.parseInt(dateAsStringArray[2]);
+        int nextDay = day;
+        int monthOfNextDay = month;
+        int yearOfNextDay = year;
+
+        // Check if year is leap year
+        if (month == 2) {
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                if (day >= 29) {
+                    nextDay = 1;
+                    monthOfNextDay++;
+                } else {
+                    nextDay++;
+                }
+            } else {
+                if (day >= 28) {
+                    nextDay = 1;
+                    monthOfNextDay++;
+                } else {
+                    nextDay++;
+                }
+            }
+
+        } else if (month >= 12) {
+            if (day >= 31) {
+                nextDay = 1;
+                monthOfNextDay = 1;
+                yearOfNextDay++;
+            }
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            if (day >= 30) {
+                nextDay = 1;
+                monthOfNextDay++;
+            }
+        } else {
+            if (day >= 31) {
+                nextDay = 1;
+                monthOfNextDay++;
+            } else {
+                nextDay++;
+            }
+        }
+
+        return (nextDay > 10 ? nextDay : "0" + nextDay) + "." +
+                (monthOfNextDay > 10 ? monthOfNextDay : "0" + monthOfNextDay) + "." + yearOfNextDay;
+    }
+
+    // Check if the date matches required format
+    public static boolean dateVerification(String date) {
+        try {
+            Pattern pattern = Pattern.compile("(?<day>\\d{2})\\.(?<month>\\d{2})\\.(\\d+$)");
+            Matcher matcher = pattern.matcher(date);
+            if (!matcher.matches()) {
+                return false;
+            }
+            int day = Integer.parseInt(matcher.group("day"));
+            int month = Integer.parseInt(matcher.group("month"));
+
+            if (day <= 0 || month <= 0 || month > 12 || day > 31) {
+                return false;
+            }
+
+        } catch (Exception exc) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
---
+++ src/test/java/com/sergdalm/week1/Task0Test.java
@@ -0,0 +1,28 @@
+package com.sergdalm.week1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Task0Test {
+    @Test
+    void checkDateVerification() {
+        Assertions.assertTrue(Task0.dateVerification("12.07.1993"));
+        Assertions.assertFalse(Task0.dateVerification("32.07.1993"));
+        Assertions.assertFalse(Task0.dateVerification("01.13.1993"));
+        Assertions.assertFalse(Task0.dateVerification("00.13.1993"));
+        Assertions.assertFalse(Task0.dateVerification("120.07.1993"));
+        Assertions.assertFalse(Task0.dateVerification("01.00.1993"));
+        Assertions.assertFalse(Task0.dateVerification("0.07.1993"));
+        Assertions.assertFalse(Task0.dateVerification("01.2.1993"));
+        Assertions.assertFalse(Task0.dateVerification("01.0o.1993"));
+    }
+
+    @Test
+    void checkNextDate(){
+
+        Assertions.assertEquals(Task0.getNextDay("31.01.2022"),"01.02.2022");
+        Assertions.assertEquals(Task0.getNextDay("31.12.2021"), "01.01.2022");
+        Assertions.assertEquals(Task0.getNextDay("28.02.2022"), "01.03.2022");
+    }
+
+}

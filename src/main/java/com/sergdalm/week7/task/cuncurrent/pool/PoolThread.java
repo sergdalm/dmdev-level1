3027c91229ff13d5f4ac06660029e2dfb3404730
Alex <30310397+sergdalm@users.noreply.github.com>
2022-06-20 18:49:13 +0300

    Added examples of using thread pools

---
+++ src/main/java/com/sergdalm/week7/task/cuncurrent/pool/PoolThread.java
@@ -0,0 +1,25 @@
+package com.sergdalm.week7.task.cuncurrent.pool;
+
+import java.util.Optional;
+import java.util.Queue;
+
+public class PoolThread extends Thread {
+    private final Queue<Runnable> tasks;
+
+    public PoolThread(Queue<Runnable> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            Optional<Runnable> task = Optional.empty();
+            synchronized (tasks) {
+                if (!tasks.isEmpty()) {
+                    task = Optional.of(tasks.remove());
+                }
+            }
+            task.ifPresent(Runnable::run);
+        }
+    }
+}
